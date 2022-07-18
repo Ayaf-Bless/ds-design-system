cf799899b67ab96c798683b1bf93e64933d01fe9
Ayaf-Bless <blessambel1@gmail.com>
2022-07-18 21:30:20 +0300

    setting rollup for the react app

---
+++ packages/react/rollup.config.js
@@ -0,0 +1,13 @@
+import TS from "rollup-plugin-typescript2";
+
+export default {
+  input: ["src/index.ts", "src/atoms/Button/index.ts"],
+  output: {
+    dir: "lib",
+    format: "esm",
+    sourcemap: true,
+  },
+  plugins: [TS()],
+  preserveModules: true,
+  external: ["react"],
+};
